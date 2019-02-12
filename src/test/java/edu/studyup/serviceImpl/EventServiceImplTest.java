ca68f4e84ee424f62e11fae32960af3d010a3320
John Sung <josung@ucdavis.edu>
2019-02-11 17:34:23 -0800

    unittest to 100% coverage

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -46,7 +47,7 @@ class EventServiceImplTest {
 		//Create Event1
 		Event event = new Event();
 		event.setEventID(1);
-		event.setDate(new Date());
+		event.setDate(new Date(2020, 12, 13));
 		event.setName("Event 1");
 		Location location = new Location(-122, 37);
 		event.setLocation(location);
@@ -62,14 +63,14 @@ class EventServiceImplTest {
 		DataStorage.eventData.clear();
 	}
 
-	@Test
+	@Test //should pass 
 	void testUpdateEventName_GoodCase() throws StudyUpException {
 		int eventID = 1;
 		eventServiceImpl.updateEventName(eventID, "Renamed Event 1");
 		assertEquals("Renamed Event 1", DataStorage.eventData.get(eventID).getName());
 	}
 	
-	@Test
+	@Test //should pass 
 	void testUpdateEvent_WrongEventID_badCase() {
 		int eventID = 3;
 		Assertions.assertThrows(StudyUpException.class, () -> {
@@ -77,7 +78,7 @@ class EventServiceImplTest {
 		  });
 	}
 	
-	@Test
+	@Test //should pass 
 	void testUpdateEvent_falseLength() {
 		int eventID = 1;
 		String name = "NewEventName                                     ";
@@ -87,7 +88,7 @@ class EventServiceImplTest {
 	}
 
 	
-	@Test
+	@Test //should pass 
 	void testUpdateEvent_twentyLength() throws StudyUpException{
 		int eventID = 1;
 		String name = "12345678901234567890";
@@ -95,14 +96,41 @@ class EventServiceImplTest {
 		assertEquals("12345678901234567890", DataStorage.eventData.get(eventID).getName());
 	}
 	
-	@Test 
-	void test_getActiveEvents() {
-		Map<Integer, Event> eventData = DataStorage.eventData;
-		List<Event> activeEvents = eventServiceImpl.getActiveEvents();
-		assertEquals(eventData.size(),activeEvents.size());
+	@Test //should pass 
+	void testUpdateEvent_LongEventID_badCase() {
+		int eventID = 1;
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.updateEventName(eventID, "Renamed this file. It is over 20 characters ijiaifgdjiodfgjiodfg");
+		  });
 	}
 	
-	@Test 
+	@Test //should pass all
+	void testgetActiveEvent() throws StudyUpException{
+		Event event = new Event();
+		event.setEventID(2);
+		event.setDate(new Date(97, 12, 13));
+		event.setName("Event 2");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		DataStorage.eventData.put(event.getEventID(), event);
+		
+		Event event3 = new Event();
+		event3.setEventID(3);
+		event3.setDate(new Date(98, 12, 13));
+		event3.setName("Event 3");
+		Location location3 = new Location(-2, 37);
+		event.setLocation(location3);
+		DataStorage.eventData.put(event3.getEventID(), event3);
+		
+		Map<Integer, Event> eventData = DataStorage.eventData;
+		List<Event> activeEvents = eventServiceImpl.getActiveEvents();
+		assertTrue(eventData.get(1).getDate().after(new Date()));
+		assertTrue(eventData.get(3).getDate().before(new Date()));
+		assertTrue(new Date(97, 12, 13).before(new Date()));
+		assertEquals(1,activeEvents.size());
+	}
+	
+	@Test //should pass
 	void testAddStudentToEvent_NegativeEventID_badCase() {
 		int eventID = -1;
 		Student student = new Student();
@@ -114,34 +142,29 @@ class EventServiceImplTest {
 			eventServiceImpl.addStudentToEvent(student,eventID);
 		  });
 	}
-	@Test
-	void testUpdateEvent_LongEventID_badCase() {
-		int eventID = 1;
-		Assertions.assertThrows(StudyUpException.class, () -> {
-			eventServiceImpl.updateEventName(eventID, "Renamed this file. It is over 20 characters ijiaifgdjiodfgjiodfg");
-		  });
-	}
-	@Test 
-	void testAddStudentToEvent_EventID_goodCase() {
+	
+	@Test //should pass
+	void testAddStudentToEvent_EventID_goodCase() throws StudyUpException{
 		int eventID = 1;
 		Student student = new Student();
 		student.setFirstName("John");
 		student.setLastName("Sung");
 		student.setEmail("josung@email.com");
 		student.setId(2);
-		Assertions.assertThrows(StudyUpException.class, () -> {
-			eventServiceImpl.addStudentToEvent(student,eventID);
-		  });
+		eventServiceImpl.addStudentToEvent(student,eventID);
+		Map<Integer, Event> eventData = DataStorage.eventData;
+		assertEquals(eventData.get(eventID).getStudents().size(), 2);
+		  
 	}
-	@Test 
-	void testAddStudentToEvent_EmptyEventID_badCase() {
+	@Test //should pass
+	void testAddStudentToEvent_EmptyEvent_badCase() throws StudyUpException{
 		int eventID = 2;
 		Student student = new Student();
 		student.setFirstName("John");
 		student.setLastName("Sung");
 		student.setEmail("josung@email.com");
 		student.setId(2);
-		//Create Event1
+		//Create Event2
 		Event event = new Event();
 		event.setEventID(2);
 		event.setDate(new Date());
@@ -150,12 +173,12 @@ class EventServiceImplTest {
 		event.setLocation(location);
 				
 		DataStorage.eventData.put(event.getEventID(), event);
-		Assertions.assertThrows(StudyUpException.class, () -> {
-			eventServiceImpl.addStudentToEvent(student,eventID);
-		  });
+		eventServiceImpl.addStudentToEvent(student,eventID);
+		Map<Integer, Event> eventData = DataStorage.eventData;
+		assertEquals(eventData.get(eventID).getStudents().size(), 1);
 	}
 	
-	@Test 
+	@Test //should pass
 	void testAddStudentToEvent_morethantwo_badcase() throws StudyUpException {
 		int eventID = 1;
 		Student student = new Student();
@@ -172,22 +195,41 @@ class EventServiceImplTest {
 	    Assertions.assertThrows(StudyUpException.class, () -> {
 			eventServiceImpl.addStudentToEvent(student2,eventID);
 		  });
-
 	}
-	@Test
-
-	void testgetActiveEvent() throws StudyUpException{
+	
+	@Test //all should pass
+	void test_getPastEvents() {
 		Event event = new Event();
 		event.setEventID(2);
-		event.setDate(new Date(2020, 12, 13));
+		event.setDate(new Date(97, 12, 13));
 		event.setName("Event 2");
 		Location location = new Location(-122, 37);
 		event.setLocation(location);
 		DataStorage.eventData.put(event.getEventID(), event);
-		List<Event> expected = new ArrayList<>();
-		expected.add(event);
-		List<Event> actual = eventServiceImpl.getActiveEvents();
-		assertTrue(DataStorage.eventData.get(1).getDate().before(new Date()));
-		assertEquals(expected, actual);
+		
+		Event event3 = new Event();
+		event3.setEventID(3);
+		event3.setDate(new Date(98, 12, 13));
+		event3.setName("Event 3");
+		Location location3 = new Location(-2, 37);
+		event.setLocation(location3);
+		DataStorage.eventData.put(event3.getEventID(), event3);
+		
+		Map<Integer, Event> eventData = DataStorage.eventData;
+		List<Event> pastEvents = eventServiceImpl.getPastEvents();
+		assertTrue(eventData.get(1).getDate().after(new Date()));
+		assertTrue(eventData.get(3).getDate().before(new Date()));
+		assertTrue(new Date(97, 12, 13).before(new Date()));
+		assertEquals(2,pastEvents.size());
 	}
+	
+	@Test //should pass
+	void test_deleteEvent() {
+		int eventID = 1;
+		eventServiceImpl.deleteEvent(eventID);
+		Map<Integer, Event> eventData = DataStorage.eventData;
+		Map<Integer, Event> expectedData = new HashMap<Integer, Event>();
+		assertEquals(expectedData.size(), eventData.size());
+	}
+	
 }
